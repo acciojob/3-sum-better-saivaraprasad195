6dacb838050a54e8020b8b5d5a47df3c164a868a
saivaraprasad195 <62549091+saivaraprasad195@users.noreply.github.com>
2024-01-18 23:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
+	let n = arr.length;
+	let closestSum=Infinity;
+	for(let i=0;i<n;i++){
+		let sum =0;
+		for(let j=i+1,k=n-1 ; j<k;j++,k--){
+			sum = arr[i]+arr[j]+arr[k];
+			if(sum == target) return target;
+
+			closestSum = Math.min(closestSum, Math.abs(sum-target));	
+		}
+	}
+
+	return closestSum;
   
 }
 
